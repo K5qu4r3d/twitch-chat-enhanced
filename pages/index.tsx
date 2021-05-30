efd33d4b69f49c6cbc4c6befeb1075132818dbaa
kkeomalaythong <kkeomalaythong117@gmail.com>
2021-05-29 20:25:35 -0400

    Set up initial Twitch API call

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,31 @@
-import React, { useState, useEffect, useRef } from 'react';
-import styled from 'styled-components'
+import React, { useState, useEffect, useRef, ReactElement } from 'react';
+import styled from 'styled-components';
+import TwitchApi from 'node-twitch';
+import { GetStaticProps } from 'next';
 
+interface IIndexProps {
+  api: string
+}
 
-export const Index = (): React.ReactElement => {
+export const getStaticProps: GetStaticProps = async () => {
+  const api = new TwitchApi({
+    client_id: `${process.env.CLIENT_ID}`,
+    client_secret: `${process.env.CLIENT_ID}`
+  });
 
+  console.log(api);
+
+  const apiString = JSON.stringify(api);
+
+  return {
+    props: {
+      api: apiString
+    }
+  };
+}
+
+export const Index = ({ api }: IIndexProps): ReactElement => {
+  console.log(JSON.parse(api));
 
   return (
     <Container>
