201bf5b878ae582faf1a7581f7526646e71fb70c
kkeomalaythong <kkeomalaythong117@gmail.com>
2021-08-26 16:22:33 -0400

    Added API directory and some functionality

--- pages/ChatMessage.tsx
+++ pages/ChatMessage.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import getThumbnails from '../src/utils/GetThumbnails';
 import { Chat } from './[streamername]';
 
 interface IChatmessageProps {
@@ -12,9 +11,6 @@ const ChatMessage: React.FunctionComponent<IChatmessageProps> = ({ messages }: I
 
 		return username ? username : '';
 	});
-
-	const thumbnailUrls = getThumbnails(usernames);
-	console.log(thumbnailUrls);
 
 	return (
 		<React.Fragment>
