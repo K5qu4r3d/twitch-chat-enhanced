9b4f9385ae0a4471ba15fae09d9909d5be0038df
kkeomalaythong <kkeomalaythong117@gmail.com>
2021-09-16 09:37:03 -0400

    First pass on putting images to chat messages

--- pages/ChatMessage.tsx
+++ pages/ChatMessage.tsx
@@ -1,57 +1,81 @@
 import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
+
 import { Chat } from './[streamername]';
 
-interface IChatmessageProps {
-	messages: Chat[]
+interface IChatMessageProps {
+	messages: Chat[];
 }
 
-const ChatMessage: React.FunctionComponent<IChatmessageProps> = ({ messages }: IChatmessageProps) => {
-	const [usernames, setUsernames] = useState<string[]>([])
-  const [thumbnailUrls, setThumbnailUrls] = useState<string[]>([]);
+export interface IMessageAndUrl {
+  chat: Chat;
+  profileImageUrl: string;
+}
 
+const ChatMessage: React.FunctionComponent<IChatMessageProps> = ({ messages }: IChatMessageProps) => {
+  const [pairs, setPairs] = useState<IMessageAndUrl[]>([]);
+
+  // TODO: maybe fetch URLs and add to messages props?
 	// fetching thumbnail URLs via node-twitch API calls
   useEffect(() => {
-    setUsernames(messages.map(message => {
-      const username = message.tags['display-name'];
-  
-      return username ? username : '';
-    }));
-
-    const getThumbnails = async () => {
-      if (usernames.length > 0) {
-        const obj = {
-          usernames: usernames
-        };
+    if (messages.length > 0) {
+      const usernames: string[] = messages.map(message => {
+        const username = message.tags['display-name'];
     
-        const response = await fetch('/api/twitch/thumbnailUrls', {
+        return username ? username : '';
+      });
+
+      const getThumbnails = async () => {
+        const response = await fetch('/api/twitch/getProfilePictureUrls', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
           },
-          body: JSON.stringify(obj)
+          body: JSON.stringify({
+            chats: messages,
+            usernames: usernames
+          })
         });
-    
-        const responseJson = await response.json();
-
-        setThumbnailUrls(responseJson['urls']);
-      }
-    };
-
-    getThumbnails();
+        
+        setPairs(await response.json());
+      };
+  
+      getThumbnails();
+    }
   }, [messages]);
-	
+
 	return (
 		<React.Fragment>
-			{messages.map((x: Chat, i: number) => {
-				const { tags, message, media } = x;
-				const { 'message-type': messageType, 'display-name': displayName, color } = tags;
+			{/* messages.map((x: Chat, i: number) => {
+          const { tags, message, media } = x;
+          const { 'message-type': messageType, 'display-name': displayName, color } = tags;
 
-				if (media) {
-					return <div key={i}><img src={media} /></div>;
-				} else {
-					return <p key={i} className="textstroke"><strong style={{ color: color }}>{displayName}</strong>: {message}</p>;
-				}
-			})}
+          if (media) {
+            return <div key={i}><img src={media} /></div>;
+          } else {
+            return (
+              <p key={i} className="textstroke">
+                <strong style={{ color: color }}>{displayName}</strong>: {message}
+              </p>
+            );
+          }
+        }) */
+        pairs.map(({ chat, profileImageUrl }: IMessageAndUrl, i: number) => {
+          const { tags, message, media } = chat;
+          const { 'message-type': messageType, 'display-name': displayName, color } = tags;
+
+          if (media) {
+            return <div key={i}><img src={media} /></div>;
+          } else {
+            return (
+              <p key={i} className="textstroke">
+                <Image src={profileImageUrl} width={50} height={50} />
+                <strong style={{ color: color }}>{displayName}</strong>: {message}
+              </p>
+            );
+          }
+        })
+        }
 		</React.Fragment>
 	);
 };
--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,11 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, json } from 'express';
 import next from 'next';
-import routes from './routes/routes';
+import TwitchApi from 'node-twitch';
+import { User } from 'node-twitch/dist/types/objects';
+import { IMessageAndUrl } from '../pages/ChatMessage';
+
+import { Chat } from '../pages/[streamername]';
+// import routes from './routes/routes';
 
 const port = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== 'production';
@@ -13,10 +18,30 @@ const handler = app.getRequestHandler();
 
     const server = express();
 
-    server.use('/api', routes(server));
+    server.use(json());
+
+    // server.use('/api', routes(server));
 
     server.get('*', (req: Request, res: Response) => {
       return handler(req, res);
+    });
+
+    server.post('/api/twitch/getProfilePictureUrls', async (req: Request, res: Response) => {
+      const chats: Chat[] = req.body['chats'];
+
+      const twitch = new TwitchApi({
+        client_id: (process.env.CLIENT_ID as string),
+        client_secret: (process.env.CLIENT_SECRET as string)
+      });
+      
+      const users = await twitch.getUsers(req.body['usernames']);
+      
+      res.send(users.data.map((user: User, index: number) => {
+        return {
+          chat: chats[index],
+          profileImageUrl: user.profile_image_url
+        };
+      }));
     });
 
     server.listen(port, (err?: any) => {
